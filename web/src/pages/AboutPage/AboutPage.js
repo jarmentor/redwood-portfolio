26ee6fca55f4b598cc5bdfddef30024b5b5652bf
Jonathan Armentor <jarmentor@me.com>
2020-05-24 23:58:29 -0500

    Update about page content

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,29 @@
 import PageLayout from '@layouts/PageLayout'
+import { Link, routes } from '@redwoodjs/router'
+import './AboutPage.css'
 const AboutPage = () => {
   return (
     <PageLayout>
-      <h1>AboutPage</h1>
-      <p>Find me in ./web/src/pages/AboutPage/AboutPage.js</p>
+      <article>
+        <section>
+          <img src="/profile_photo.jpg" alt="" className="profilePhoto" />
+          <h1>Hi, I'm Jonathan</h1>
+          <p className="lead">
+            I'm a designer and developer focused on crafting meaningful user
+            experiences through visual identities and digital design.
+          </p>
+        </section>
+        <section>
+          <h2>Looking to start a new project?</h2>
+          <p>
+            <Link to={routes.contact()}>Contact me</Link> or{' '}
+            <a href="mailto:jonathan@jonathanarmentor.com">
+              send me an e-mail.
+            </a>{' '}
+            I'm looking forward to making something new with you.
+          </p>
+        </section>
+      </article>
     </PageLayout>
   )
 }
