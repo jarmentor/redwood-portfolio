d6151e575b712cd47c4c4b2f4fe076c264673ead
Jonathan Armentor <jarmentor@me.com>
2020-05-24 21:32:12 -0500

    Contact Page Styling

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,20 +1,68 @@
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+} from '@redwoodjs/web'
+import './ContactPage.css'
 import PageLayout from '@layouts/PageLayout'
-import InputText from '@components/InputText'
+
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <PageLayout>
       <h1>Contact</h1>
-      <form action="/">
-        <InputText name="first-name">First Name</InputText>
-        <input
-          type="submit"
-          value="submit"
-          onClick={() => {
-            alert('blue')
+      <h2 className="subhead">
+        Proident eiusmod exercitation esse irure Lorem.
+      </h2>
+      <Form
+        className="contactForm"
+        validation={{ mode: 'onBlur' }}
+        onSubmit={onSubmit}
+      >
+        <label htmlFor="name" errorClassName="error">
+          Name
+        </label>
+        <TextField
+          className="input"
+          name="name"
+          errorClassName="error"
+          validation={{ required: true }}
+        />
+        <FieldError name="name" className="error" />
+
+        <label htmlFor="email" errorClassName="error">
+          E-Mail
+        </label>
+        <TextField
+          className="input"
+          name="email"
+          errorClassName="error"
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+\..+/,
+            },
+            message: 'Please enter a valid e-mail address.',
           }}
         />
-      </form>
-      <p>Find me in ./web/src/pages/ContactPage/ContactPage.js</p>
+        <FieldError name="email" className="error" />
+
+        <label htmlFor="message" errorClassName="error">
+          Message
+        </label>
+        <TextAreaField
+          name="message"
+          errorClassName="error"
+          validation={{ required: true, message: 'please enter a message' }}
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit className="submit">Submit</Submit>
+      </Form>
     </PageLayout>
   )
 }
