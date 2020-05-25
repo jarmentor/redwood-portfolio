b6eb70821b796cb51c52225f0960c5bc5b5204e7
Jonathan Armentor <jarmentor@me.com>
2020-05-24 23:59:38 -0500

    Add placeholder content to HomePage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import PageLayout from 'src/layouts/PageLayout'
-
+import { Link, routes } from '@redwoodjs/router'
 const HomePage = () => (
   <PageLayout>
-    <h1>Welcome Home</h1>
-    <h2>you dirty rat bastard.</h2>
-    <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+    <h1>Welcome</h1>
+    <p>
+      This site is still in active development. However, the{' '}
+      <Link to={routes.contact()}>contact form</Link> functions properly.
+    </p>
   </PageLayout>
 )
 
