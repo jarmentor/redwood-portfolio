e97a91f63a53f2c2d3f39e02f5e1e8c438f0272d
Jonathan Armentor <jarmentor@me.com>
2020-05-27 19:53:20 -0500

    replaced component CSS with lineria CSS in JS

--- web/src/components/Gallery/Gallery.js
+++ web/src/components/Gallery/Gallery.js
@@ -1,7 +1,16 @@
-import './Gallery.css'
+import { css } from 'linaria'
+
+const galleryStyles = css`
+  display: grid;
+  max-width: 80%;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 2em;
+  grid-template-rows: auto;
+`
+
 const Gallery = ({ images }) => {
   return (
-    <div className="ImageGallery">
+    <div className={galleryStyles}>
       {images.map((image, counter) => (
         <img key={counter} src={image} width="200" height="200" />
       ))}
