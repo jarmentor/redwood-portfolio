49f22b78b4d08117dac3fc6449a61085428e3b70
Jonathan Armentor <jarmentor@me.com>
2020-05-27 19:53:37 -0500

    replace all layout CSS with lineria css-in-js

--- web/src/layouts/FooterLayout/FooterLayout.js
+++ web/src/layouts/FooterLayout/FooterLayout.js
@@ -1,9 +1,27 @@
 import Masthead from '@components/Masthead'
 import Copyright from '@components/Copyright'
-import './FooterLayout.css'
+import { css } from 'linaria'
+
+const footerStyles = css`
+  align-items: flex-start;
+  background: var(--brand-primary);
+  color: var(--midnight-green);
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 5em 3em;
+
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+  }
+
+  h1 {
+    margin-top: 0;
+  }
+`
 
 const FooterLayout = ({ title = 'Jonathan Armentor', children }) => (
-  <footer>
+  <footer className={footerStyles}>
     <Masthead title={title} />
     <Copyright holder={title} />
     {children}
