fc12d7f4ff2470f2c8714261b8f59e9b1745d308
Jonathan Armentor <jarmentor@me.com>
2020-06-05 15:47:29 -0500

    seperate admin routes
    isolates the admin routes to their own variable

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,24 +1,27 @@
 import { Router, Route, Private } from '@redwoodjs/router'
 
+const AdminRoutes = () => (
+  <Private unauthenticated="home">
+    <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+
+    <Route
+      path="/admin/posts/{id:Int}/edit"
+      page={EditPostPage}
+      name="editPost"
+    />
+    <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+    <Route path="/admin/posts" page={PostsPage} name="posts" />
+  </Private>
+)
+
 const Routes = () => (
   <Router>
-    <Private unauthenticated="home">
-      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-
-      <Route
-        path="/admin/posts/{id:Int}/edit"
-        page={EditPostPage}
-        name="editPost"
-      />
-      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-      <Route path="/admin/posts" page={PostsPage} name="posts" />
-    </Private>
     <Route path="/project" page={ProjectPage} name="project" />
     <Route path="/projects" page={ProjectsPage} name="projects" />
     <Route path="/about" page={AboutPage} name="about" />
     <Route path="/contact" page={ContactPage} name="contact" />
     <Route path="/typography" page={TypographyPage} name="typography" />
-
+    <AdminRoutes />
     <Route path="/" page={HomePage} name="home" />
     <Route notfound page={NotFoundPage} />
   </Router>
