321a7fc43b6be7e587cec4009a7e0e0e4300170d
Jonathan Armentor <jarmentor@me.com>
2020-06-05 15:49:55 -0500

    refine form control styles
    refine the structure of the contact form styles
    moves styles to a more CSS-in-JS friendly approach

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -11,54 +11,6 @@ import { css } from 'linaria'
 
 import { PageLayout } from '@layouts'
 
-const contactFormStyles = css`
-  display: flex;
-  flex-direction: column;
-
-  input,
-  textarea {
-    border: none;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.16), 0 1px 3px rgba(0, 0, 0, 0.23);
-    max-width: 25em;
-    font-size: 1.125rem;
-    padding: 0.625rem;
-    color: var(--midnight-green);
-    transition: ease-in-out 150ms;
-    will-change: box-shadow;
-    border-radius: 5px;
-    :focus {
-      outline: 0;
-      box-shadow: 0 7px 14px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
-    }
-  }
-  label {
-    margin-top: 4rem;
-    margin-bottom: 1rem;
-    :after {
-      display: inline;
-      content: ':';
-    }
-  }
-
-  .error {
-    border: var(--bittersweet);
-    color: var(--bittersweet);
-    font-size: 0.8rem;
-    margin-top: 1rem;
-  }
-`
-const submitStyles = css`
-  background: var(--midnight-green);
-  border-radius: 0.125em;
-  border: 0;
-  box-shadow: 0px;
-  color: white;
-  letter-spacing: 0.125em;
-  margin-top: 2em;
-  max-width: 10em;
-  padding: 1em 0.75em;
-  text-transform: uppercase;
-`
 const CREATE_CONTACT = gql`
   mutation CreateContactMutation($input: CreateContactFormInput!) {
     createContactForm(input: $input) {
@@ -71,7 +23,6 @@ const ContactPage = () => {
   const formMethods = useForm()
   const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
     onCompleted: () => {
-      // TODO: Change success options.
       alert('Thank you for your submission!')
       formMethods.reset()
     },
@@ -81,9 +32,10 @@ const ContactPage = () => {
     create({ variables: { input: data } })
   }
   return (
-    <PageLayout>
-      <h1>Contact</h1>
-      <h2 className="subhead">Start a new project or just drop me a line!</h2>
+    <PageLayout
+      header="Contact"
+      subhead="Start a new project or just drop me a line!"
+    >
       <Form
         className={contactFormStyles}
         validation={{ mode: 'onBlur' }}
@@ -91,24 +43,32 @@ const ContactPage = () => {
         error={error}
         formMethods={formMethods}
       >
-        <label htmlFor="name" errorClassName="error">
+        <label
+          className={labelStyles}
+          htmlFor="name"
+          errorClassName={errorStyles}
+        >
           Name
         </label>
         <TextField
           className="input"
           name="name"
-          errorClassName="error"
+          errorClassName={errorStyles}
           validation={{ required: true }}
         />
-        <FieldError name="name" className="error" />
+        <FieldError name="name" className={errorStyles} />
 
-        <label htmlFor="email" errorClassName="error">
+        <label
+          className={labelStyles}
+          htmlFor="email"
+          errorClassName={errorStyles}
+        >
           E-Mail
         </label>
         <TextField
           className="input"
           name="email"
-          errorClassName="error"
+          errorClassName={errorStyles}
           validation={{
             required: true,
             pattern: {
@@ -117,9 +77,13 @@ const ContactPage = () => {
             message: 'Please enter a valid e-mail address.',
           }}
         />
-        <FieldError name="email" className="error" />
+        <FieldError name="email" className={errorStyles} />
 
-        <label htmlFor="message" errorClassName="error">
+        <label
+          className={labelStyles}
+          htmlFor="message"
+          errorClassName={errorStyles}
+        >
           Message
         </label>
         <TextAreaField
@@ -127,7 +91,7 @@ const ContactPage = () => {
           errorClassName="error"
           validation={{ required: true, message: 'please enter a message' }}
         />
-        <FieldError name="message" className="error" />
+        <FieldError name="message" className={errorStyles} />
 
         <Submit disabled={loading} className={submitStyles}>
           Submit
@@ -137,4 +101,52 @@ const ContactPage = () => {
   )
 }
 
+const contactFormStyles = css`
+  display: flex;
+  flex-direction: column;
+
+  input,
+  textarea {
+    border-radius: 5px;
+    border: none;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.16), 0 1px 3px rgba(0, 0, 0, 0.23);
+    color: var(--midnight-green);
+    font-size: 1.125rem;
+    max-width: 25em;
+    padding: 0.625rem;
+    transition: ease-in-out 150ms;
+    will-change: box-shadow;
+    :focus {
+      outline: 0;
+      box-shadow: 0 7px 14px rgba(0, 0, 0, 0.25), 0 5px 5px rgba(0, 0, 0, 0.22);
+    }
+  }
+`
+const labelStyles = css`
+  margin-top: 4rem;
+  margin-bottom: 1rem;
+  &:after {
+    display: inline;
+    content: ':';
+  }
+`
+
+const errorStyles = css`
+  border: var(--bittersweet);
+  color: var(--bittersweet);
+  font-size: 0.8rem;
+  margin-top: 1rem;
+`
+const submitStyles = css`
+  background: var(--black);
+  border-radius: 0.125em;
+  border: 0;
+  box-shadow: 0px;
+  color: var(--off-white);
+  letter-spacing: 0.125em;
+  margin-top: 2em;
+  max-width: 10em;
+  padding: 1em 0.75em;
+  text-transform: uppercase;
+`
 export default ContactPage
