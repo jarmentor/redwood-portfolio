1ea133c8cd6062d9712ac803a82f60a1972a9a60
Jonathan Armentor <jarmentor@me.com>
2020-06-05 15:45:41 -0500

    Update colors.js
    updated colors js
    add black value
    change yellow value
    change offWhite value

--- web/src/styles/colors.js
+++ web/src/styles/colors.js
@@ -1,11 +1,12 @@
 const colors = {
+  black: '#63616c',
   midnightGreen: '#1a535c',
   mediumTurquoise: '#4ecdc4',
   mintCream: '#f7fff7',
   white: '#fff',
   bittersweet: '#ff6b6b',
-  yellow: '#ffe66d',
-  offWhite: '#e5e5e5',
+  yellow: '#ffcd67',
+  offWhite: '#fffef0',
 }
 
 export default colors
