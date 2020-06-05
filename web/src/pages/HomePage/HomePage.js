f1b610e10f9afdaed69b3f2e761669e7140ca43c
Jonathan Armentor <jarmentor@me.com>
2020-06-05 15:51:06 -0500

    Use lorem component for test content
    use lorem component for test content in home page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,24 +1,16 @@
-import { useAuth } from '@redwoodjs/auth'
+import golden from '/src/lib/golden'
 
+import { Lorem } from '@testContent'
 import { PageLayout } from '@layouts'
-const HomePage = () => {
-  const { logIn, isAuthenticated, logOut, currentUser } = useAuth()
-  return (
-    <PageLayout>
-      <h1>Hello.</h1>
-      <p>
-        Exercitation amet velit dolor qui veniam aute exercitation qui culpa.
-        Sint duis ad non consequat sint qui Lorem quis exercitation voluptate et
-        aute in. Pariatur proident officia non mollit id{' '}
-        <a href="#">anim laborum qui voluptate</a> proident mollit duis
-        excepteur consectetur. Ex dolor occaecat deserunt proident anim officia
-        occaecat irure nulla.
-      </p>
+import { Authenticated } from '@components'
 
-      <a href="#" onClick={isAuthenticated ? logOut : logIn}>
-        {isAuthenticated ? `Log Out (${currentUser.email})` : 'Log In'}
-      </a>
-    </PageLayout>
-  )
-}
+const HomePage = () => (
+  <PageLayout header="Hello!">
+    <h2>Upper: {golden(12, 'up')}</h2>
+    <h2>Lower: {golden(12)}</h2>
+    <Lorem paragraphs="3" />
+    <Authenticated />
+  </PageLayout>
+)
+
 export default HomePage
