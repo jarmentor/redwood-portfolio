8b07ef89e1ce16612c0e149e7678eae282e31b29
Jonathan Armentor <jarmentor@me.com>
2020-06-07 20:49:38 -0500

    ..

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,19 +1,5 @@
 import { Router, Route, Private } from '@redwoodjs/router'
 
-const AdminRoutes = () => (
-  <Private unauthenticated="home">
-    <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-
-    <Route
-      path="/admin/posts/{id:Int}/edit"
-      page={EditPostPage}
-      name="editPost"
-    />
-    <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-    <Route path="/admin/posts" page={PostsPage} name="posts" />
-  </Private>
-)
-
 const Routes = () => (
   <Router>
     <Route path="/project" page={ProjectPage} name="project" />
@@ -21,7 +7,17 @@ const Routes = () => (
     <Route path="/about" page={AboutPage} name="about" />
     <Route path="/contact" page={ContactPage} name="contact" />
     <Route path="/typography" page={TypographyPage} name="typography" />
-    <AdminRoutes />
+    <Private unauthenticated="home">
+      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+
+      <Route
+        path="/admin/posts/{id:Int}/edit"
+        page={EditPostPage}
+        name="editPost"
+      />
+      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/admin/posts" page={PostsPage} name="posts" />
+    </Private>
     <Route path="/" page={HomePage} name="home" />
     <Route notfound page={NotFoundPage} />
   </Router>
